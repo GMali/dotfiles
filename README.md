1adb598859e4ccc3b8fffaddd0b13e9e399ffe6f
Gaurav Mali <dev@gauravmali.com>
2025-03-10 19:17:44 +0700

    build(backup): add backup script + update README
    Now we can just run `./backup.sh` to copy all configurations and
    preferences to this repo.

--- README.md
+++ README.md
@@ -1,20 +1,65 @@
-#  New Environment Setup
+# README
 
-1. Install [zsh](https://ohmyz.sh/), and the plugins listed in `.zshrc`
-2. Install [iterm](https://iterm2.com/), and the profile exports in the `iterm/` folder
-3. Move all dotfiles and `.vim` folder to `~/`
+**Table of Contents**
+- [README](#readme)
+  - [How to setup a new machine](#how-to-setup-a-new-machine)
+    - [Download and setup iTerm2](#download-and-setup-iterm2)
+    - [Zsh](#zsh)
+    - [Vim](#vim)
+    - [Gitconfig](#gitconfig)
+    - [Copy all other .files](#copy-all-other-files)
+    - [Homebrew](#homebrew)
+  - [How to backup changes to this repo](#how-to-backup-changes-to-this-repo)
 
-## Git Config
+## How to setup a new machine
+
+### Download and setup iTerm2
+
+1. You can find install instructions on [iterm website](https://iterm2.com/).
+2. Install the fonts found in the `iterm/fonts/` folder.
+3. Set up dynamic profiling and copy the profiles from the `iterm/` folder.
+4. If that doesn't automatically set up the colors, import them manually from the `iterm/` folder.
+
+
+### Zsh
+
+1. You can find install instructions on [zsh website](https://ohmyz.sh/)
+2. Look at the plugins listed in `.zshrc` and install them manually.
+
+```
+cp .p10k.zsh ~/
+```
+
+### Vim
+
+```
+cp .vimrc ~/
+cp -R .vim/ ~/
+```
+
+### Gitconfig
+
+```
+cp .gitconfig ~/
+```
+
 Create a `.localgitconfig` file with the new email address
 
-## Homebrew packages
+### Copy all other .files
 
-To backup:
 ```
-brew leaves > my_brews.txt
+cp .aliases .exports .functions ~/
 ```
 
-To install:
+### Homebrew
+
 ```
 xargs brew install < my_brew.txt
 ```
+
+## How to backup changes to this repo
+
+```
+./backup.sh
+````
+Then commit and push changes
---
+++ backup.sh
@@ -0,0 +1,97 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+# ------------------------------------------------------------------
+# This script copies your configuration files and directories to the
+# current working directory, and exports your brew leaves (installed
+# formulae) while filtering out unwanted ones (as listed in the
+# "blacklist_formulae" file).
+# ------------------------------------------------------------------
+
+printColorful() {
+    printf "\033[38;2;%sm%s\033[0m" "26;188;156" "$1..."
+}
+
+printDone() {
+    printf "✅\n"
+}
+
+copyFiles() {
+    for file in "${files_to_copy[@]}"; do
+        if [[ -f "$file" ]]; then
+            cp "$file" .
+        else
+            printColorful "Warning: $file does not exist."
+        fi
+    done
+}
+
+copyDirectories() {
+    for dir in "${directories_to_copy[@]}"; do
+        if [[ -d "$dir" ]]; then
+            cp -R "$dir" .
+        else
+            printColorful "Warning: $dir does not exist."
+        fi
+    done
+}
+
+copyProfiles() {
+    target_dir="iterm"
+    mkdir -p "$target_dir"
+    iterm_file="$HOME/Library/Application Support/iTerm2/DynamicProfiles/property_list.json"
+    if [[ -f "$iterm_file" ]]; then
+        cp "$iterm_file" "$target_dir/"
+    else
+        printColorful "Warning: $iterm_file does not exist."
+    fi
+}
+
+exportBrew() {
+    brew_leaves=$(brew leaves)
+    blacklist="blacklist_formulae.txt"
+    if [[ -f "$blacklist" ]]; then
+        printColorful "Filtering out blacklisted formulae from $blacklist"
+        echo "$brew_leaves" | grep -v -F -f "$blacklist" >my_brew.txt
+    else
+        printColorful "No blacklist file found. Using all brew leaves."
+        echo "$brew_leaves" >my_brew.txt
+    fi
+}
+
+# 1. Files to copy from your home directory:
+files_to_copy=(
+    "$HOME/.p10k.zsh"
+    "$HOME/.aliases"
+    "$HOME/.exports"
+    "$HOME/.functions"
+    "$HOME/.gitconfig"
+    "$HOME/.vimrc"
+    "$HOME/.zshrc"
+)
+
+printColorful "Copying .files"
+copyFiles
+printDone
+
+# 2. Directories to copy:
+directories_to_copy=(
+    "$HOME/.vim"
+)
+
+printColorful "Copying .directories"
+copyDirectories
+printDone
+
+# 3. Copy iTerm2 dynamic profiles file into an "iterm" directory.
+# Create the target directory if it doesn't exist.
+printColorful "Copying iTerm2 profiles"
+copyProfiles
+printDone
+
+# 4. Export currently installed brew formulae and filter out blacklisted ones.
+printColorful "Exporting brew leaves"
+exportBrew
+printDone
+
+echo -e "\033[1;32m$(cowsay 'Backup complete ^_^')\033[0m"
